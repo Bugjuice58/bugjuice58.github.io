4a98d780ecaf7d1113aea7b0bed3fa03bf611ea2
KyleF01 <89658838+KyleF01@users.noreply.github.com>
2022-12-20 10:48:45 -0700

    Fix

--- led/sketch.js
+++ led/sketch.js
@@ -604,8 +604,8 @@ let locationSuccess = false;
 function getCurrentSunTime() {
     if (locationSuccess) {
         // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${coords.latitude}&lon=${coords.longitude}&appid=${weatherAPIKEY}`);
-         .then((response) => response.json());
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${coords.latitude}&lon=${coords.longitude}&appid=${weatherAPIKEY}`)
+         .then((response) => response.json())
          .then((data) => weatherData=JSON.parse(data));
          sunriseTime = weatherData.current.sunrise;
          sunsetTime = weatherData.current.sunset;
