5c0d4bd02a40935a59d8f93e10215013c5d5bbb8
Kyū <59759353+Bugjuice58@users.noreply.github.com>
2023-04-23 21:48:40 -0600

    yay

--- ily2/script.js
+++ ily2/script.js
@@ -1,10 +1,16 @@
 let ilyPercent = 1
+let intervalReset = false
 
 function addNew() {
     let newIly = document.createElement("p");
     newIly.textContent = `${ilyPercent}%`
     document.querySelector("body").appendChild(newIly);
     ilyPercent++
+    if (ilyPercent >= 5000 && intervalReset) {
+        clearInterval(interval);
+        interval = setInterval(addNew, 50);
+        intervalReset = true;
+    }
 }
 
 let interval = setInterval(addNew,5)
